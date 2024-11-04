3814b2e1e1df2b7662f6b88fde128f7ea70f0fb6
ryusei <acs112146@gm.ntcu.edu.tw>
2024-11-04 21:37:30 +0800

    CH2.2 new

---
+++ CH2.2/eleven.java
@@ -0,0 +1,19 @@
+
+
+
+import java.util.Scanner;
+
+public class eleven {
+    
+    public static void main(String[] args)
+    {
+        Scanner keyin = new Scanner(System.in);
+        int a;
+        int b;
+        a = keyin.nextInt();
+        b = keyin.nextInt();
+        System.out.print(a+b);
+        keyin.close(); 
+
+    }
+}
